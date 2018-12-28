73ad12e0001ed52cd6b66c491283e14d3222691c
LongWinter <ye.yuanemail@gmail.com>
2018-12-27 21:55:11 -0600

    define engines for heroku(specify npm version, node version); and define server start script

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+// enable dynamic port binding
+// if there is any environment variable that is predifined, then use that environment variable as our port
+// Otherwise, use 5000!
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("we are listening on :" + PORT);
+});
